3a486e22ef886fd11d87849fabc540f9962bf4a1
rubex87 <rubenhuetor@gmail.com>
2020-10-28 13:22:52 +0100

    Update
    Actualización del proyeccto

---
+++ Proyecto/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animation anim;
+    
+   
+
+    
+  
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+         rb.velocity = new Vector2(-5,rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+         rb.velocity = new Vector2(5,rb.velocity.y);
+         transform.localScale = new Vector2 (1, 1);
+
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+         rb.velocity = new Vector2(rb.velocity.x, 10f);
+
+        }
+    }
+}
