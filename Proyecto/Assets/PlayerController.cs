d29c419559d645792ad658243423b95d36e8ee71
rubex87 <rubenhuetor@gmail.com>
2020-11-09 23:48:15 +0100

    actualizacion
    Actualizacion

--- Proyecto/Assets/PlayerController.cs
+++ Proyecto/Assets/PlayerController.cs
@@ -5,36 +5,71 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idle,running,jumping}
+    private State state =State.idle;
     
+   
+
+
+private void Start (){
+
+    rb = GetComponent<Rigidbody2D>();
+    anim = GetComponent <Animator>();
+
+}
+
 
   
     // Update is called once per frame
     private void Update()
+
     {
-        if(Input.GetKey(KeyCode.A))
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if(hDirection < 0)
         {
             rb.velocity = new Vector2(-5,rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
+            
         }
         
-       else if(Input.GetKey(KeyCode.D))
+       else if(hDirection > 0)
         {
          rb.velocity = new Vector2(5,rb.velocity.y);
          transform.localScale = new Vector2 (1, 1);
-         anim.SetBool("running", true);
+         
         }
         else
         {
-            anim.SetBool("running",false);
+            
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
          rb.velocity = new Vector2(rb.velocity.x, 10f);
+         state = State.jumping;
+        }
+
+        VelocityState();
+        
+    }
+
+    private void VelocityState()
+
+    {
+        if(state == State.jumping){
+
+        }
+
+        else if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
+{
+    //moving
+    state = State.running;
+ 
+}
 
         }
     }
-}
+
